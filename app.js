59729ecb7f6e480d3a93bcf8a9bf45f26747fc24
Matto <matt.mcquain@gmail.com>
2020-01-11 19:23:46 -0800

    initialized the Movie model

--- app.js
+++ app.js
@@ -5,9 +5,17 @@ const sequelize = new Sequelize({
   storage: 'movies.db'
 });
 
-// Defines the async function
-// async IIFE
-// uses a try...catch statement to catch all exceptions thrown. We pass catch() an error param to contain the error details 
+// Movie model
+// We extend from Sequelize.Model, which is part of Sequelize's API for model definition. 
+// To initialize and configure the model, we call static class init() method on the model name (Movie). 
+// Movie.init() defines a new table in the DB w/ the name 'Movies'. Sequelize will look for information in the 'Movies' table. 
+// REMEMBER! The model name is singular and the table name is plural. Don't worry, Sequelize uses inflection to auto pluralize the table name. 
+//
+class Movie extends Sequelize.Model {}
+Movie.init();
+
+// Defines the async function. async IIFE
+// Uses a try...catch statement to catch all exceptions thrown. We pass catch() an error param to contain the error details. 
 // await sequelize.authenticate() tests the connection 
 // 
 (async () => {
