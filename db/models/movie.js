0ade72d80409af3f22db7a87932004208759ffde
Matto <matt.mcquain@gmail.com>
2020-01-19 20:57:28 -0800

    set custom primary key

--- db/models/movie.js
+++ db/models/movie.js
@@ -3,7 +3,27 @@ const Sequelize = require('sequelize');
 module.exports = (sequelize) => {
   class Movie extends Sequelize.Model {}
   Movie.init({
-    title: Sequelize.STRING,
+    // Sets custom primary key column
+    // "primaryKey: true" tells Sequelize to generate the primary key column using the property name defined in the model (for us its 'id').
+    // The ID should be a number, so our data tpe is INTEGER.
+    // "autoIncrement: true" auto generates an ID that increments by 1 for each new entry.
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: Sequelize.STRING
+    },
+    runtime: {
+      type: Sequelize.INTEGER
+    },
+    releaseDate: {
+      type: Sequelize.DATEONLY
+    },
+    isAvailableOnVHS: {
+      type: Sequelize.BOOLEAN
+    },
   }, {sequelize});
 
   return Movie;
